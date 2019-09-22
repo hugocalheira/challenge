b7d3337ec0a23b7035aed3f3c55b1c44dc978cd1
Hugo C. Durães <hugocalheira@gmail.com>
2019-09-22 11:34:36 -0300

    Loading

--- react/src/components/AlbumsList/AlbumsList.js
+++ react/src/components/AlbumsList/AlbumsList.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 
+import { FaSpinner } from 'react-icons/fa';
 import { Content, List } from './albumsList.styles';
 import AlbumCard from '../AlbumCard/AlbumCard';
 
-export default function AlbumsList({ title, data, type }) {
+export default function AlbumsList({ title, data, type, loading }) {
     return (
-        <Content>
+        <Content isLoading={loading}>
             <h2>{title}</h2>
-            <List>
-                {data.map(item => (
-                    <AlbumCard key={item.id} item={item} type={type} />
-                ))}
-            </List>
+            {!loading ? (
+                <List>
+                    {data.map(item => (
+                        <AlbumCard key={item.id} item={item} type={type} />
+                    ))}
+                </List>
+            ) : (
+                <FaSpinner color="#999" size={32} />
+            )}
         </Content>
     );
 }
 
 AlbumsList.propTypes = {
     title: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired,
     data: PropTypes.arrayOf(PropTypes.any).isRequired,
+    type: PropTypes.string.isRequired,
+    loading: PropTypes.bool,
+};
+
+AlbumsList.defaultProps = {
+    loading: false,
 };
