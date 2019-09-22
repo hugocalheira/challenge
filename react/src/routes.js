1ec499507e329a0a74a76c9ba90bb45c123435b4
Hugo C. Durães <hugocalheira@gmail.com>
2019-09-22 09:09:53 -0300

    Authentication service

--- react/src/routes.js
+++ react/src/routes.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Albums from './pages/Albums';
+import { isAuthenticated } from './services/auth';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+        {...rest}
+        render={props => isAuthenticated() && <Component {...props} />}
+    />
+);
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/albums" component={Albums} />
+                <PrivateRoute path="/" exact component={Home} />
+                <PrivateRoute
+                    path="/albums"
+                    component={props => <Albums {...props} />}
+                />
+                <PrivateRoute
+                    path="/callback"
+                    component={() => {
+                        return <Redirect to="/" />;
+                    }}
+                />
             </Switch>
         </BrowserRouter>
     );
