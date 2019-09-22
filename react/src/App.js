343ba62885a77b47c5c47d3e4c0cfb3e68901189
Hugo C. Durães <hugocalheira@gmail.com>
2019-09-22 09:09:53 -0300

    Authentication service

--- react/src/App.js
+++ react/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FaSpotify } from 'react-icons/fa';
+// import axios from 'axios';
 import Routes from './routes';
 
 import GlobalStyle from './styles/global';
+import { isAuthenticated } from './services/auth';
 
 export default function App() {
+    useEffect(() => {
+        isAuthenticated();
+    }, []);
+
     return (
         <>
             <span className="logo">
--- react/src/services/api.js
+++ react/src/services/api.js
@@ -4,4 +4,8 @@ const api = axios.create({
     baseURL: 'https://api.spotify.com/v1',
 });
 
+export const auth = axios.create({
+    baseURL: 'https://accounts.spotify.com/api/token',
+});
+
 export default api;
