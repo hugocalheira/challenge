0900238a23b914667eb5eb8ffd3d33e45bd5f3cc
Hugo C. Durães <hugocalheira@gmail.com>
2019-09-22 17:34:51 -0300

    Configuring jest

--- react/src/services/auth.js
+++ react/src/services/auth.js
@@ -1,4 +1,4 @@
-export default async function authenticate() {
+export async function authenticate() {
     const urlCallback = 'http://localhost:8080/callback';
     const clientId = '4803f7e865be449e8ec8d31777e1af47';
 
@@ -13,7 +13,9 @@ export default async function authenticate() {
     }
 
     if (!hashParams.access_token) {
-        window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=${urlCallback}`;
+        window.location.replace(
+            `https://accounts.spotify.com/authorize?client_id=${clientId}&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=${urlCallback}`
+        );
     } else {
         sessionStorage.setItem('access_token', hashParams.access_token);
     }
