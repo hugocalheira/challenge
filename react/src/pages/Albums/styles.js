49eb55b37041739cf6379a25d35d07882fef2f2d
Hugo C. Durães <hugocalheira@gmail.com>
2019-09-23 08:04:00 -0300

    Playing sample track

--- react/src/pages/Albums/styles.js
+++ react/src/pages/Albums/styles.js
@@ -18,33 +18,39 @@ export const Back = styled.div`
 `;
 
 export const Content = styled.div`
-    color: #fafafa;
-    font-size: 16px;
-    font-weight: normal;
     margin: 1rem 0.5rem;
     display: flex;
 
-    ol {
-        /* list-style: decimal; */
-        list-style: none;
-        counter-reset: li;
+    div {
+        counter-reset: button;
         margin: 0.25rem 0.5rem;
         flex: 1;
 
-        li {
-            margin-left: 6rem;
+        button {
+            color: #fafafa;
+            font-size: 16px;
+            font-weight: normal;
+            border: 0;
+            width: 100%;
+            background: none;
+            margin-left: 5rem;
             padding: 0.75rem;
-            counter-increment: li;
+            counter-increment: button;
             display: flex;
+            flex-wrap: wrap;
             justify-content: space-between;
             cursor: pointer;
 
             &::before {
-                content: counter(li) '.';
+                content: counter(button) '.';
                 color: #999;
                 position: absolute;
                 width: 1em;
                 margin-left: -3em;
+            }
+
+            .control {
+                display: none;
             }
 
             .time {
@@ -52,10 +58,17 @@ export const Content = styled.div`
                 margin-left: 0.5rem;
             }
 
-            &:hover span:first-child {
-                filter: drop-shadow(0px 0px 5px black);
-                transform: translateX(-10px);
-                transition: all 0.3s;
+            &.playing,
+            &:hover {
+                .control {
+                    display: inline-block;
+                    font-size: 14px;
+                }
+                span:first-child {
+                    filter: drop-shadow(0px 0px 5px black);
+                    transform: translateX(-10px);
+                    transition: all 0.3s;
+                }
             }
 
             span:first-child {
