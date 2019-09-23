33e44d00de40f977de3ee5916ee649a6aeadbea0
Hugo C. Durães <hugocalheira@gmail.com>
2019-09-23 00:19:44 -0300

    Picture component

--- react/src/components/AlbumCard/AlbumCard.js
+++ react/src/components/AlbumCard/AlbumCard.js
@@ -1,60 +1,23 @@
 import React from 'react';
-import { PropTypes } from 'prop-types';
-import { FaMusic } from 'react-icons/fa';
+import PropTypes from 'prop-types';
 
 import { Item } from './albumCard.styles';
+import Picture from '../Picture/Picture';
 
 export default function AlbumCard({ item, type }) {
     return (
         <Item>
-            {type === 'artist' && (
-                <div className="card">
-                    {item.images.length ? (
-                        <img alt={item.name} src={item.images[1].url} />
-                    ) : (
-                        <div className="notAvailable">
-                            <FaMusic />
-                        </div>
-                    )}
-                    <p>{item.name}</p>
-                </div>
-            )}
-
-            {type === 'album' && (
-                <div className="card">
-                    {item.images.length ? (
-                        <img alt={item.name} src={item.images[1].url} />
-                    ) : (
-                        <div className="notAvailable">
-                            <FaMusic />
-                        </div>
-                    )}
-                    <p>{item.name}</p>
-                    <span>{item.artists[0].name}</span>
-                </div>
-            )}
-
-            {type === 'track' && (
-                <div className="card">
-                    {item.album.images.length ? (
-                        <img
-                            alt={item.album.name}
-                            src={item.album.images[1].url}
-                        />
-                    ) : (
-                        <div className="notAvailable">
-                            <FaMusic />
-                        </div>
-                    )}
-                    <p>{item.name}</p>
-                    <span>{item.album.name}</span>
-                </div>
-            )}
+            <div className="card">
+                <Picture item={item} type={type} />
+                <p>{item.name}</p>
+                {type === 'album' && <span>{item.artists[0].name}</span>}
+                {type === 'track' && <span>{item.album.name}</span>}
+            </div>
         </Item>
     );
 }
 
 AlbumCard.propTypes = {
     type: PropTypes.string.isRequired,
-    // data: PropTypes.arrayOf(PropTypes.any).isRequired,
+    item: PropTypes.object.isRequired,
 };
