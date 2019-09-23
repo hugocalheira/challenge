ee6a4863ad83758f1edec61afc175c8219bf1876
Hugo C. Durães <hugocalheira@gmail.com>
2019-09-23 08:04:00 -0300

    Playing sample track

--- react/src/pages/Albums/index.js
+++ react/src/pages/Albums/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FaChevronLeft } from 'react-icons/fa';
+import { FaChevronLeft, FaPlay, FaPause } from 'react-icons/fa';
 import AlbumCard from '../../components/AlbumCard/AlbumCard';
 import api from '../../services/api';
 import { isAuthenticated } from '../../services/auth';
@@ -9,6 +9,7 @@ import { Back, Content } from './styles';
 
 export default function Albums({ match }) {
     const [album, setAlbum] = useState(false);
+    const [playing, setPlaying] = useState(false);
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
@@ -41,6 +42,22 @@ export default function Albums({ match }) {
         return `${minutes}:${`0${seconds}`.slice(-2)}`;
     }
 
+    async function handleClick(track) {
+        const { id, preview_url: url } = track;
+        const audio = await new Audio(url);
+        if (playing) {
+            playing.audio.pause();
+        }
+
+        if (playing === false || audio.src !== playing.audio.src) {
+            setPlaying({ id, audio });
+            audio.play();
+            audio.addEventListener('ended', () => setPlaying(false));
+            return;
+        }
+        setPlaying(false);
+    }
+
     return (
         <>
             <Back>
@@ -52,18 +69,35 @@ export default function Albums({ match }) {
                 {!loading && album && (
                     <>
                         <AlbumCard type="album" item={album} plusSize />
-                        <ol>
+                        <div>
                             {album.tracks.items.map(track => (
-                                <li key={track.id}>
+                                <button
+                                    className={
+                                        playing && playing.id === track.id
+                                            ? 'playing'
+                                            : ''
+                                    }
+                                    type="button"
+                                    key={track.id}
+                                    onClick={() => handleClick(track)}
+                                >
                                     <span>{track.name}</span>
                                     <span className="time">
+                                        <span className="control">
+                                            {playing &&
+                                            playing.id === track.id ? (
+                                                <FaPause />
+                                            ) : (
+                                                <FaPlay />
+                                            )}
+                                        </span>
                                         {millisecondsToMinutesAndSeconds(
                                             track.duration_ms
                                         )}
                                     </span>
-                                </li>
+                                </button>
                             ))}
-                        </ol>
+                        </div>
                     </>
                 )}
             </Content>
